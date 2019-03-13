73c2322790a68acfce4479bca8c7bda4a8360852
Bobby 'Bobdawg' Grayson <bobbbygrayson@gmail.com>
2019-03-13 11:55:27 -0400

    use one for all supervisor strategy

--- lib/counter/application.ex
+++ lib/counter/application.ex
@@ -15,7 +15,7 @@ defmodule Counter.Application do
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :rest_for_one, name: Counter.Supervisor]
+    opts = [strategy: :one_for_all, name: Counter.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
